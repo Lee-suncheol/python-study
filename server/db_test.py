fe289fb8f6f6fc06da58be712ae866aa3bc8d206
leesunchul81@gmail.com <leesunchul81@gmail.com>
2021-05-11 17:51:11 +0900

    4. Flask

---
+++ server/db_test.py
@@ -0,0 +1,16 @@
+import repository
+
+# 1.INSERT
+
+
+data = {"title":"title2","content":"content1"}
+#repository.save(data)
+
+# 2. SELECT
+#repository.findAll()
+
+#repository.findById(4)
+
+# DELETE
+repository.deleteById(4)
+
